ab03bed4875ed4b4d0f7de2da7f289beb8e0040f
SandCaByte <sandcm55@gmail.com>
2024-10-28 14:17:03 -0500

    Created basic ASP.Net core web application

---
+++ ASP.NetWebDemo/ASP.NetWebDemo/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NetWebDemo
+{
+    public class TodoItem
+    {
+        //Class Fields
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+
+    }
+}
